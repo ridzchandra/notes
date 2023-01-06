a26b58d686d4d20494610e5d9667323a453e0016
Ridz Balla <ridz@payble.com.au>
2023-01-06 18:41:30 +1100

    Adding a storage stack

--- services/functions/lambda.js
+++
@@ -1,7 +0,0 @@
-export const handler = async (event) => {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: `Hello, New World! Your request was received at ${event.requestContext.time}.`,
-  };
-};
